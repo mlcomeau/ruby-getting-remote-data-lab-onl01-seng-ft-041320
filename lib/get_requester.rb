5b86321c001361d9fc34bb23d7c602ef12a3881a
Megan Comeau <mlcomeau99@gmail.com>
2020-04-30 20:34:39 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
-# Write your code here
+class GetRequester 
+  def initialize(string_url)
+    @string_url = string_url 
+  end 
+
+  def get_response_body
+    uri = URI.parse(string_url)
+    response = Net::HTTP.get_response(uri)
+    response.body 
+  end 
+
+  def parse_json 
+    programs = JSON.parse(self.get_response_body)
+    programs.collect do |program|
+      program
+    end 
+  end 
+  
+  
+end 
+
