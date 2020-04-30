807f658c1308f56f069c034bdc9f560a042440ee
Megan Comeau <mlcomeau99@gmail.com>
2020-04-30 20:36:25 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  attr_accessor :string_url
+  def initialize(string_url)
+    @string_url = string_url
+  end
+
+  def get_response_body
+    uri = URI.parse(string_url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    programs = JSON.parse(self.get_response_body)
+    programs.collect do |program|
+      program
+    end
+  end
+
+
+end
